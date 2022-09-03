b8d517780d150cee0dd168b871d6e6fe07b42b98
Nathan Clevenger <nateclev@gmail.com>
2022-09-03 10:09:23 -0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,13 @@
+export default {
+  fetch: async (req, env) => {
+    const {origin, hostname, pathname, search, hash} = new URL(req.url)
+    const data = await fetch(req.url.replace(hostname + '/',''), req).then(res => res.json())
+    
+    return new Response(JSON.stringify({
+    api: {
+    },
+    data,
+    user,
+    }))
+  }
+}
