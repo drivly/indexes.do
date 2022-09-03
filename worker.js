9c58d9a105b8e4c5d21f8901fbc8782904a246a9
Nathan Clevenger <nateclev@gmail.com>
2022-09-03 10:44:20 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,26 @@
 export default {
   fetch: async (req, env) => {
     const {origin, hostname, pathname, search, hash} = new URL(req.url)
-    const data = await fetch(req.url.replace(hostname + '/',''), req).then(res => res.json())
+    const target = req.url.replace(hostname + '/',''
+    const data = await fetch(target, req).then(res => res.json())
+    
+    const indexes = Object.entries(flatten(data)).reduce((acc, [key, value]) => ({...acc, [`${key}: ${value} -> ${target}`]: target))
+
     
     return new Response(JSON.stringify({
       api: {
+        icon: '🔎',
+        name: 'indexes.do',
+        description: 'Index Generation Service',
+        url: 'https://indexes.do',
+        api: 'https://indexes.do/api',
+        endpoints: {
+          data: 'https://indexes.do/:url',
+        },
+        type: 'https://apis.do/data',
+        repo: 'https://github.com/drivly/indexes.do',
       },
+      indexes,
       data,
       user,
     }))
